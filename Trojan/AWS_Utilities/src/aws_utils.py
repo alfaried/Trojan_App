7b389a7537b4c6d171a21967312dd5157d008676
Al <faried_94@hotmail.com>
2018-09-07 00:31:45 +0800

    *

--- Trojan/AWS_Utilities/src/aws_utils.py
+++ Trojan/AWS_Utilities/src/aws_utils.py
@@ -5,8 +5,10 @@ def getInstanceID():
     response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
     instance_id = response.text
 
-    return instance_id, {'instance_id':instance_id}
+    return instance_id
 
-def getInstanceName(instance_id):
+def getInstance(instance_id):
     ec2 = boto3.resource('ec2')
-    
+    instance = ec2.Instance(instance_id)
+
+    return instance
--- Trojan/AWS_Utilities/views.py
+++ Trojan/AWS_Utilities/views.py
@@ -6,35 +6,48 @@ from AWS_Utilities.src.aws_utils import *
 from botocore.exceptions import ClientError
 
 def test(request):
-    response = {'status':'OK', 'code':'200'}
+    response = {'HTTPStatus':'OK', 'HTTPStatusCode':'200'}
     return JsonResponse(response)
 
 def killInstance(request):
-    response = {'status':'OK', 'code':'200'}
+    response = {'HTTPStatus':'OK', 'HTTPStatusCode':'200'}
 
-    instance_id,results = getInstanceID()
+    instance_id = getInstanceID()
     ec2 = boto3.client('ec2')
 
     # Do a dryrun first to verify permissions
     try:
         ec2.stop_instances(InstanceIds=[instance_id], DryRun=True)
     except ClientError as e:
-        response['status'] = 'Unauthorized'
-        response['code'] = '401'
-        response['message'] = 'Dry run failed'
-        response['error'] = e.args[0]
+        response['HTTPStatus'] = 'Unauthorized'
+        response['HTTPStatusCode'] = '401'
+        response['Message'] = 'Dry run failed'
+        response['Error'] = e.args[0]
 
     # Dry run succeeded, call stop_instances without dryrun
     try:
         results = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
         response.update(results)
     except ClientError as e:
-        response['status'] = 'Bad request'
-        response['code'] = '400'
-        response['error'] = e.args[0]
+        response['HTTPStatus'] = 'Bad request'
+        response['HTTPStatusCode'] = '400'
+        response['Error'] = e.args[0]
 
     return JsonResponse(response)
 
 def getInstanceInfo(request):
-    response = {'status':'OK', 'code':'200'}
+    response = {'HTTPStatus':'OK', 'HTTPStatusCode':'200'}
+
+    instance = getInstance()
+
+    response['instance_id'] = instance.instance_id
+    response['instance_public_ip'] = instance.public_ip_address
+    response['instance_public_dns_name'] = instance.public_dns_name
+    response['instance_type'] = instance.instance_type
+    response['instance_kernel_id'] = instance.kernel_id
+    response['instance_security_groups'] = instance.security_groups
+    response['instance_state'] = instance.state
+    response['instance_subnet_id'] = instance.subnet_id
+    response['instance_tags'] = instance.tags
+
     return JsonResponse(response)
