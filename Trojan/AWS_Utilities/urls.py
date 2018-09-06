1b1b1cb1cd08bd4bbd451dd8c5940fdc6d17f2f2
Al <faried_94@hotmail.com>
2018-09-07 01:19:29 +0800

    *

--- Trojan/AWS_Utilities/urls.py
+++ Trojan/AWS_Utilities/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.test, name='test'),
     path('get_instance_info/', views.getInstanceInfo, name='getInstanceInfo'),
-    path('get_cloudwatch_metric/', views.getInstanceInfo, name='getInstanceInfo'),
+    path('get_cloudwatch_metric/', views.getCloudWatchInfo, name='getCloudWatchInfo'),
     path('kill_instance/', views.killInstance, name='killInstance'),
 ]
