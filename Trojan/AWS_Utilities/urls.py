fd3f975832231c13ea1dd429e99df0d6280f0229
Al <faried_94@hotmail.com>
2018-09-07 22:58:32 +0800

    *

--- Trojan/AWS_Utilities/urls.py
+++ Trojan/AWS_Utilities/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.test, name='test'),
     path('ec2/instance/info/', views.getInstanceInfo, name='getInstanceInfo'),
     path('ec2/instance/kill/', views.killInstance, name='killInstance'),
+    path('cloudwatch/metric/', views.getCloudWatchMetric, name='getCloudWatchMetric'),
     path('cloudwatch/metric/info/', views.getCloudWatchMetricInfo, name='getCloudWatchMetricInfo'),
 ]
