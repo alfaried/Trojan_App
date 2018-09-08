38f43c9de252c1537d45b509364d564dee32f151
Al <faried_94@hotmail.com>
2018-09-08 14:40:52 +0800

    *

--- Trojan/AWS_Utilities/views.py
+++ Trojan/AWS_Utilities/views.py
@@ -90,7 +90,7 @@ def cloudwatch_getMetric(request,attempts=0):
 
         dimensions = metric.dimensions
         endTime = datetime.utcnow()
-        startTime = (endTime - timedelta(days=1))
+        startTime = (endTime - timedelta(days=2))
 
         statistics = metric.get_statistics(
             Dimensions=dimensions,
