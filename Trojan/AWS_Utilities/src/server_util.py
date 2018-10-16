7bd06be319863eea77f64bcb89ecae84b6499454
Al <faried_94@hotmail.com>
2018-10-16 21:35:56 +0800

    added relevant comments (No Major updates).

--- Trojan/AWS_Utilities/src/server_util.py
+++ Trojan/AWS_Utilities/src/server_util.py
@@ -1,7 +1,6 @@
 import os
 import shlex, subprocess
 
-# Return output : tuple, error : default-None
 def executeBash(bashCommand):
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     return process.communicate()
