a01ecbafdb5e612fcbae337cc2414a5d47ff21c3
Al <faried_94@hotmail.com>
2018-10-16 21:20:54 +0800

    Update bash commands

--- Trojan/AWS_Utilities/src/server_util.py
+++ Trojan/AWS_Utilities/src/server_util.py
@@ -16,26 +16,26 @@ def addPublicKeyToFile(public_key,filepath):
 
 def addAWSCredentialsToFile(access_key,secret_access_key,filepath):
     # User profile
-    bashCommand = 'sudo bash -c "echo [deafult] >> ' + filepath + '"'
+    bashCommand = 'sudo bash -c "echo [deafult] > ' + filepath + '"'
     executeSudoBash(bashCommand)
 
     # Access Key
-    bashCommand = 'sudo bash -c "echo ' + access_key + ' > ' + filepath + '"'
+    bashCommand = 'sudo bash -c "echo ' + access_key + ' >> ' + filepath + '"'
     executeSudoBash(bashCommand)
 
     # Secret Access Key
-    bashCommand = 'sudo bash -c "echo ' + secret_access_key + ' > ' + filepath + '"'
+    bashCommand = 'sudo bash -c "echo ' + secret_access_key + ' >> ' + filepath + '"'
     executeSudoBash(bashCommand)
 
 def addAWSConfigToFile(region_name,output_format,filepath):
     # User profile
-    bashCommand = 'sudo bash -c "echo [deafult] >> ' + filepath + '"'
+    bashCommand = 'sudo bash -c "echo [deafult] > ' + filepath + '"'
     executeSudoBash(bashCommand)
 
     # Region Name
-    bashCommand = 'sudo bash -c "echo ' + region_name + ' > ' + filepath + '"'
+    bashCommand = 'sudo bash -c "echo ' + region_name + ' >> ' + filepath + '"'
     executeSudoBash(bashCommand)
 
     # Output File Format
-    bashCommand = 'sudo bash -c "echo ' + output_format + ' > ' + filepath + '"'
+    bashCommand = 'sudo bash -c "echo ' + output_format + ' >> ' + filepath + '"'
     executeSudoBash(bashCommand)
