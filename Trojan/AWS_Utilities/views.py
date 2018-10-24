38995c7ce0305b29d9648dea290b4bd66a4c682a
Al <faried_94@hotmail.com>
2018-10-24 12:22:19 +0800

    Opps removed extra bracket

--- Trojan/AWS_Utilities/views.py
+++ Trojan/AWS_Utilities/views.py
@@ -904,7 +904,7 @@ def git_Pull(requests):
     response = {'HTTPStatus':'OK', 'HTTPStatusCode':200}
 
     try:
-        response['Bash_Response'] = gitCommands('pull')[0].decode('utf-8').strip())
+        response['Bash_Response'] = gitCommands('pull')[0].decode('utf-8').strip()
         response['Message'] = 'Successfully git pull'
     except Exception as e:
         traceback.print_exc()
